8bb95905ed87522fbf365c59011163ae494ce799
nick95a <nick95a@yandex.ru>
2022-09-21 13:23:32 +0500

    Update staya-auth-wrong-password.js

--- cypress/integration/4-PracticeTests/staya-auth-wrong-password.js
+++ cypress/integration/4-PracticeTests/staya-auth-wrong-password.js
@@ -9,8 +9,8 @@ describe('Авторизация с неверным паролем', function()
 	it('Открываем главную страницу staya', function() {
 		cy.visit('https://staya.dog/');
 		cy.get('a.header-bottom__right--link').click();
-        cy.get('form[method="post"] > input[name="email"]').type(email);
-        cy.get('form[method="post"] > input[name="password"]').type(password);
+        	cy.get('form[method="post"] > input[name="email"]').type(email);
+        	cy.get('form[method="post"] > input[name="password"]').type(password);
 		cy.get('form[method="post"] > button[type="submit"]').click();
 		cy.end();
 	})
