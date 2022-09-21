c389b6953e5fae40565d4d30960db37021ad3bec
nick95a <nick95a@yandex.ru>
2022-09-21 13:23:16 +0500

    Update staya-auth-wrong-password.js
    Поправил тест, чтобы в поля данные вводились через переменные.

--- cypress/integration/4-PracticeTests/staya-auth-wrong-password.js
+++ cypress/integration/4-PracticeTests/staya-auth-wrong-password.js
@@ -1,9 +1,16 @@
+// Зарегистрируйтесь на сайте staya.
+// Введите существующий имейл и некорректый пароль в переменные ниже.
+
+let email = "";
+let password = "";
+
+
 describe('Авторизация с неверным паролем', function() {
 	it('Открываем главную страницу staya', function() {
 		cy.visit('https://staya.dog/');
 		cy.get('a.header-bottom__right--link').click();
-        cy.get('form[method="post"] > input[name="email"]').type('nick95a@yandex.ru');
-        cy.get('form[method="post"] > input[name="password"]').type('12356780');
+        cy.get('form[method="post"] > input[name="email"]').type(email);
+        cy.get('form[method="post"] > input[name="password"]').type(password);
 		cy.get('form[method="post"] > button[type="submit"]').click();
 		cy.end();
 	})
